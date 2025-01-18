d8329336cebcaee002340c0991bccec24b67c975
Rico Pajarola <rp@servium.ch>
2025-01-18 13:09:55 -0500

    create testcase textproto next to the test data file

--- fingerprint/maketestcase.sh
+++ fingerprint/maketestcase.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 
 for filename in "${@}"; do
-  tcfile=$(basename "${filename}").textproto
-  tcfile="${tcfile// /_}"
-
+  tcfile="${filename}.textproto"
   if [ -f "${tcfile}" ]; then
     continue
   fi
+  sourcefile=$(basename "${filename}")
   echo creating "${tcfile}"
 
   cat<<EOF>"${tcfile}"
-source_file: "${filename}"
-
-want_fingerprint {
-	want_kind: "EXIFModelSerialPhotoID"
-	want_hash: "fixme"
-	want_quality: 0
-}
-want_fingerprint {
-	want_kind: "XMPDocumentID"
-	want_hash: "fixme"
-	want_quality: 0
-}
-
-exif {
-	want_camera_model: "fixme"
-	want_camera_serial: "fixme"
-	want_photo_id: "fixme"
-}
-
-xmp {
-	want_document_id: "fixme"
-}
+source_file: "${sourcefile}"
 EOF
 
 done
