065ab793378926bbe7c0c1faf3adf89c4d829195
Rico Pajarola <rp@servium.ch>
2025-01-26 01:16:13 -0800

    script to get testdata from web

---
+++ get_testdata.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git annex init || echo "git annex not initialized"
+
+urlbase=https://cave.servium.ch/github/dedup
+git annex list | while read where fname; do
+  case "${where}:${fname}" in
+  [X_]*:*testdata*)
+    echo "${fname}"
+    git annex addurl --file "${fname}" "${urlbase}/${fname}"
+    ;;
+  esac
+done
