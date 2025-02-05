e27a73243bb39e4684e6dc53365c52587d982436
Rico Pajarola <rp@servium.ch>
2025-02-04 23:59:56 -0800

    skip and remove updated test files with no diff

--- fingerprint/update_testcases.sh
+++ fingerprint/update_testcases.sh
@@ -1,6 +1,10 @@
 for f in *testdata/*new; do
 	ORIG="${f%%.new}"
 	UPDATED="${f}"
+	if diff -q "${ORIG}" "${UPDATED}"; then
+		rm "${UPDATED}"
+		continue
+	fi
 	diff -u -w "${ORIG}" "${UPDATED}"
 	mv -i "${UPDATED}" "${ORIG}"
 done
